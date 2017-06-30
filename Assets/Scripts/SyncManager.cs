73d3ff27e5c5f788c79815a780dc2607b49de6fc
Lars <larsarnebeck@aol.com>
2017-06-30 15:06:37 +0200

    projct added

---
+++ Assets/Scripts/SyncManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncManager : NetworkBehaviour
+{
+    [SyncVar] Vector3 sPosition;
+    [SyncVar] Quaternion sRotation;
+    [SyncVar] Vector3 sScale;
+
+	void Update () {
+	    if (isServer)
+	    {
+	        sPosition = transform.parent.position;
+	        sRotation = transform.parent.localRotation;
+	        sScale = transform.parent.localScale;
+	    }
+	    else
+	    {
+            transform.parent.position = sPosition;
+            transform.parent.localRotation = sRotation;
+            transform.parent.localScale = sScale;
+        }
+    }
+}
